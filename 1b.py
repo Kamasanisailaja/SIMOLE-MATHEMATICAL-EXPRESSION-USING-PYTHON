1a5c6322e0eb46f22508a630b07f5a75b23312c8
Kamasanisailaja <106640335+Kamasanisailaja@users.noreply.github.com>
2023-06-01 19:25:27 +0530

    Add files via upload

---
+++ 1b.py
@@ -0,0 +1,13 @@
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+c=int(input("enter the value of c:"))
+x=int(input("enter the value of x:"))
+e=int(input("enter the value of e:"))
+k=int(input("enter the value of k:"))
+t=int(input("enter the value of t:"))
+
+print("solution1=",a*x*x+b*x+c)
+print("solution2=",a*x*x*x*x+b*x*x+c)
+print("solution3=",(a*x-b)/(a*x)*b)
+print("solution1=",pow((b*e),(k*t)))
+
